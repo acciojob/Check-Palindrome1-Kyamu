c513b59fdb6fa81e1733b3045e3b91d5a7fc661e
Kyamu <124717126+Kyamu@users.noreply.github.com>
2023-03-23 01:41:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,35 @@
 
 function palindrome(str){
 
+	 var l=str.length;
+		  var start=0;
+		  var end=l-1;
+		  char arr[]=new char[l];
+		  for(var i=0;i<l;i++)
+			  {
+				  arr[i]=str.charAt(i);
+				  if(arr[i]>='A' && arr[i]<='Z')
+				  {
+					  arr[i]=(char)(arr[i]+32);
+				  }
+			  }
+		  while(start<end)
+			  {
+				  while(start<end && (arr[start]<'a' || arr[end]>'z'))
+					  {
+						  start++;
+					  }
+				  while(start<end && (arr[end]<'a' || arr[start]>'z'))
+					  {
+						  end--;
+					  }
+				  if(start<end && arr[start]!=arr[end])
+				  {
+					  return false;
+				  }
+				  start=start+1;
+				  end=end-1;
+			  }
+		  return true;
 }
 module.exports = palindrome
