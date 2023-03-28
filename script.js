ee4752877b908f6efa2ed61447aa1782ee7a0fb1
Kyamu <124717126+Kyamu@users.noreply.github.com>
2023-03-28 17:45:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 function palindrome(s){
 
 	 // get the total length of the words  
-  s = s.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+  s = s.toLowerCase();
 
   // Check if the string is the same forwards and backwards
   for (let i = 0; i < Math.floor(s.length / 2); i++) {
